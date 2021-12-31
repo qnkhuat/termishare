d5ebe21c373eabd139218e27aac2ea75e8a0d6c6
Ngoc Khuat <qn.khuat@gmail.com>
2021-12-31 13:43:08 +0700

    fix small text on mobile

--- www/src/termishare/pages/index.cljs
+++ www/src/termishare/pages/index.cljs
@@ -4,9 +4,9 @@
 (defn index
   []
   [:div {:class "w-screen h-screen flex items-center justify-center bg-black"}
-   [:a {:class "text-white text-center font-bold text-xl  mb-24
+   [:a {:class "text-white text-center font-bold text-xl mb-24
                decoration-2 decoration-lime-500 underline decoration-wavy underline-offset-4 hover:underline-offset-8"
         :href "https://github.com/qnkhuat/termishare"}
-    [GitHubIcon {:class "mb-2 animate-bounce"}]
+    [GitHubIcon {:class "mb-2 animate-bounce" :fontSize "large"}]
     [:br]
     "github.com/qnkhuat/termishare"]])
