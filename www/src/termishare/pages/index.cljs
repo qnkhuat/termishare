25158f06de128ddf2fd18fcbdd6765e2c78470d0
Ngoc Khuat <qn.khuat@gmail.com>
2021-12-31 13:32:41 +0700

    bouncing

--- www/src/termishare/pages/index.cljs
+++ www/src/termishare/pages/index.cljs
@@ -7,6 +7,6 @@
    [:a {:class "text-white text-center font-bold text-xl  mb-24
                decoration-2 decoration-lime-500 underline decoration-wavy underline-offset-4 hover:underline-offset-8"
         :href "https://github.com/qnkhuat/termishare"}
-    [GitHubIcon {:class "mb-2"}]
+    [GitHubIcon {:class "mb-2 animate-bounce"}]
     [:br]
     "github.com/qnkhuat/termishare"]])
