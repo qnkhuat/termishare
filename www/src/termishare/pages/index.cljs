f8707520becaaeb182a51c8a0cf707d7abd79020
Ngoc Khuat <qn.khuat@gmail.com>
2021-12-31 13:16:52 +0700

    beautifful home page that direct to github

--- www/src/termishare/pages/index.cljs
+++ www/src/termishare/pages/index.cljs
@@ -5,7 +5,7 @@
   []
   [:div {:class "w-screen h-screen flex items-center justify-center bg-black"}
    [:a {:class "text-white text-center font-bold text-xl  mb-24
-               decoration-sky-500 hover:decoration-sky-200 decoration-2 underline decoration-wavy underline-offset-4"
+               decoration-2 decoration-lime-500 underline decoration-wavy underline-offset-4 hover:underline-offset-8"
         :href "https://github.com/qnkhuat/termishare"}
     [GitHubIcon {:class "mb-2"}]
     [:br]
