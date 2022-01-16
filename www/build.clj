e3c315b1af05566459b061168941b049e408ee24
Ngoc Khuat <qn.khuat@gmail.com>
2022-01-16 11:34:41 +0700

    build both front-end and back-end in one command

--- www/build.clj
+++ www/build.clj
@@ -1,17 +1,24 @@
 (ns build
-  (:require [clojure.tools.build.api :as b]))
+  (:require [clojure.tools.build.api :as b]
+            [clojure.java.shell :refer [sh]]))
 
 (def class-dir "target/classes")
 (def src-dir "src/server/")
 (def basis (b/create-basis {:project "deps.edn"}))
 (def uber-file "target/colab.jar")
 
+(defn release-frontend
+  []
+  (sh "npm" "install")
+  (sh "npm" "run" "release"))
+
 (defn clean [_]
   (b/delete {:path "target"}))
 
 (defn uber [_]
-  ;(clean nil)
+  (clean nil)
 
+  (release-frontend)
   (b/copy-dir {:src-dirs [src-dir "resources"]
                :target-dir class-dir})
 
